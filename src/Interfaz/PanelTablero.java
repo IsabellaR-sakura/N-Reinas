e3dd6d205bc8c61a1ea9c444563d978941cf6a9d
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-08 18:13:03 -0500

    [FIX]: Quitando función para importar la imagen de reina, simplemente se deja con el string

--- src/Interfaz/PanelTablero.java
+++ src/Interfaz/PanelTablero.java
@@ -11,18 +11,6 @@ public class PanelTablero extends JPanel {
     public PanelTablero(int N) {
         this.N = N;
         setPreferredSize(new Dimension(700, 700));
-        try {
-            var stream = getClass().getResourceAsStream("/chess-36310_1280.png");
-            if (stream == null) stream = getClass().getResourceAsStream("chess-36310_1280.png");
-            if (stream == null) {
-                System.out.println("ERROR: imagen no encontrada");
-            } else {
-                reinaImg = ImageIO.read(stream);
-                System.out.println("OK: imagen cargada " + reinaImg.getWidth(null) + "px");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public void setSolucion(int[] solucion) {
@@ -42,20 +30,14 @@ public class PanelTablero extends JPanel {
                 g.fillRect(col * casilla, fila * casilla, casilla, casilla);
 
                 if (solucion != null && solucion[fila] == col) {
-                    if (reinaImg != null) {
-                        g.drawImage(reinaImg,
-                            col * casilla + 5, fila * casilla + 5,
-                            casilla - 10, casilla - 10, this);
-                    } else {
-                        int fontSize = (int)(casilla * 0.7);
-                        g.setFont(new Font("Serif", Font.PLAIN, fontSize));
-                        FontMetrics fm = g.getFontMetrics();
-                        String q = "♛";
-                        int x = col * casilla + (casilla - fm.stringWidth(q)) / 2;
-                        int y = fila * casilla + (casilla - fm.getHeight()) / 2 + fm.getAscent();
-                        g.setColor(Color.BLACK);
-                        g.drawString(q, x, y);
-                    }
+                	int fontSize = (int)(casilla * 0.7);
+                    g.setFont(new Font("Serif", Font.PLAIN, fontSize));
+                    FontMetrics fm = g.getFontMetrics();
+                    String q = "♛";
+                    int x = col * casilla + (casilla - fm.stringWidth(q)) / 2;
+                    int y = fila * casilla + (casilla - fm.getHeight()) / 2 + fm.getAscent();
+                    g.setColor(Color.BLACK);
+                    g.drawString(q, x, y);
                 }
             }
         }
